a11e631fb41f1d2d5fa2d920d90a827e446d7ddf
AsterinGray <raymondtiandinatan@gmail.com>
2020-05-17 09:47:20 +0700

    qty selector fixed

--- js/shop.js
+++ js/shop.js
@@ -1,14 +1,36 @@
-function minus() {
-    let value = parseInt($("#value").text());
-    if (value != 1) {
-        $("#value").text(value-1);
+function incrementValue(e) {
+    e.preventDefault();
+    var fieldName = $(e.target).data('field');
+    var parent = $(e.target).closest('div');
+    var currentVal = parseInt(parent.find('input[name=' + fieldName + ']').val(), 10);
+  
+    if (!isNaN(currentVal)) {
+      parent.find('input[name=' + fieldName + ']').val(currentVal + 1);
+    } else {
+      parent.find('input[name=' + fieldName + ']').val(0);
     }
-}
-
-function plus() {
-    let value = parseInt($("#value").text());
-    $("#value").text(value+1);
-}
+  }
+  
+  function decrementValue(e) {
+    e.preventDefault();
+    var fieldName = $(e.target).data('field');
+    var parent = $(e.target).closest('div');
+    var currentVal = parseInt(parent.find('input[name=' + fieldName + ']').val(), 10);
+  
+    if (!isNaN(currentVal) && currentVal > 0) {
+      parent.find('input[name=' + fieldName + ']').val(currentVal - 1);
+    } else {
+      parent.find('input[name=' + fieldName + ']').val(0);
+    }
+  }
+  
+  $('.qty').on('click', '.button-plus', function(e) {
+    incrementValue(e);
+  });
+  
+  $('.qty').on('click', '.button-minus', function(e) {
+    decrementValue(e);
+  });
 
 function review() {
     if ($(".review-form").height()<=0) {
